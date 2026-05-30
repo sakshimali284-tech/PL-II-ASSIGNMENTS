5e8c1461ff0a82e7d13a9aa5ccd7bade1d5aef98
sakshimali284-tech <shwetmali284@gmail.com>
2026-05-29 22:37:28 -0700

    Create Assignment 5.py

---
+++ Assignment 5.py
@@ -0,0 +1,17 @@
+#To study built in functions of string and math packages
+#Assignment 5
+#1. WAP to Calculate length of string
+a = input("Enter a string: ")
+print("Length of String is: ",len(a))
+
+#2. WAP to make string from first two and last two characters from given string
+b = input("Enter a string: ")
+c = b[:2]
+d = b[-2:]
+e = c + d
+print(e)
+
+#3. WAP to concatenate two strings
+x = "Hello "
+y = "World!"
+print(x+y)
