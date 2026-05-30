b8ee4c0fc9e7d8f7f3f95fff8f396107ed44d2d5
sakshimali284-tech <shwetmali284@gmail.com>
2026-05-29 22:34:35 -0700

    Create Assignment 2.py

---
+++ Assignment 2.py
@@ -0,0 +1,44 @@
+#to study arithematic, relational, and logical operators in python
+#assiignment no. 2
+#312
+#1 WAP to add two numbers in python
+a=5
+b=10
+c=a+bprint(c)
+
+#2. WAP to declare variables and display types of respective variables
+o=10 
+print(o,":", type(o))
+p=2.5
+print(p,":", type(p))
+q='Hello world!'
+print(q,":", type(q))
+r=True 
+print(r,":", type(r))
+s=[] 
+print(s,":", type(s))
+t=()
+print(t,":", type(t))
+u={}
+print(u,":", type(u))
+
+
+#3. WAP to demonstrate type casting in python 
+a=5 
+b=10
+d=a/b
+print("before type anything:")
+print(d)
+print("\n after type anything:")
+print(int(d))
+
+
+#4. Wap to demonstrate logical
+e=True
+f=False
+g=e and f
+print(g)
+h=e or f
+print(h)
+i=not e
+print(i)
