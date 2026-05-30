8c7ff9e3a8da262f83c6a46cf5d439d9e5c930b7
sakshimali284-tech <shwetmali284@gmail.com>
2026-05-29 22:33:22 -0700

    Update and rename README.md to Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,20 @@
+#demonstrate basic data types in python
+#assignment 1 
+#312
+#Q.A write a python program to demonstrate the following data type integer, float, string, boolean, list, tuple, dictionary, set
+a=10 
+print(a,":", type(a))
+b=2.5
+print(b,":", type(b))
+c='Hello world!'
+print(c,":", type(c))
+d=True 
+print(d,":", type(d))
+e=[] 
+print(e,":", type(e))
+f=()
+print(f,":", type(f))
+g={}
+print(g,":", type(g))
+h=set()
+print(h,":", type(h))
